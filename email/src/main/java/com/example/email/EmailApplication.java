55e3053cbb06552a1970a2be96862dcbb0b70ce6
zhujch <zhujch@yonyou.com>
2018-11-21 09:36:28 +0800

    email

--- email/src/main/java/com/example/email/EmailApplication.java
+++ email/src/main/java/com/example/email/EmailApplication.java
@@ -1,9 +1,12 @@
 package com.example.email;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.email.dao")
 public class EmailApplication {
 
 	public static void main(String[] args) {
--- email/src/main/java/com/example/email/controller/LoginController.java
+++ email/src/main/java/com/example/email/controller/LoginController.java
@@ -1,20 +1,25 @@
 package com.example.email.controller;
 
 import com.example.email.bean.login;
-import com.example.email.dao.LoginDao;
+import com.example.email.service.Loginservice;
+import com.example.email.service.Categoryservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class LoginController {
     @Autowired
-    private LoginDao loginDao;
+    private Loginservice loginservice;
+    @Autowired
+    private Categoryservice Categoryservice;
     @RequestMapping("main")
     public String login(){
         return "login";
@@ -23,29 +28,63 @@ public class LoginController {
 //@ResponseBody
     public String Login(Model m, @RequestParam("username") String username, @RequestParam("password") String password){
     List<login> list=new ArrayList<>();
-    list=loginDao.findAll();
+    list=loginservice.findAll();
+   Map<Integer, String> categories = Categoryservice.listByMap();
     for (login log:list) {
-          if(username.equals(log.getUsername())) {
-            if(password.equals(log.getPassword())){
-                return "mail";
-            }else{
-                m.addAttribute("username",username);
-              m.addAttribute("err","密码错误！");
-              return "login";
-            }
-          }else {
-              m.addAttribute("err1","用户名不存在！");
-             return "login";
-          }
+          if(username.equals(log.getUsername()) && password.equals(log.getPassword())) {
+              m.addAttribute("username",username);
+              m.addAttribute("categories",categories);
+                return "home";
+//            }else{
+//                m.addAttribute("username",username);
+//              m.addAttribute("err","密码错误！");
+//              return "login";
+//            }
+//          }else {
+//              m.addAttribute("err1","用户名不存在！");
+//             return "login";
+//         }
     }
-    return null;
+    }
+        m.addAttribute("err","用户名或密码错误！");
+        return "login";
 }
 
 
 
     @RequestMapping("register")
-    //注册aaaaaaaaaaa
-    public String register(){
-        return "register";
+     //@ResponseBody
+    public String register(Model m, login list) {
+        //login log = JSON
+        List<login> list1=new ArrayList<>();
+         list1 =  loginservice.findAll();
+
+            if (list.getUsername() == ""|| list.getPassword() == "") {
+                m.addAttribute("err1", "用户名或密码不能为空！");
+                return "register";
+            } else  {
+                for(login login : list1){
+                    if((list.getUsername()).equals(login.getUsername())){
+                        m.addAttribute("err2","用户名已存在！");
+                        return "register";
+                    }
+                }
+                loginservice.save(list);
+                return "login";
+
+            }
+
+        }
+
+
+
+    @RequestMapping("Register")
+ public String Register(){
+     return "register";
+  }
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("username");
+        return "login";
     }
 }
