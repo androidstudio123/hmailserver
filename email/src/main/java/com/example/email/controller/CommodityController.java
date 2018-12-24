ac0c84dc9bd071b39c9e8ac5a24a3ce4791212a4
zhujch <zhujch@yonyou.com>
2018-12-24 20:54:25 +0800

    bb

--- email/src/main/java/com/example/email/controller/CommodityController.java
+++ email/src/main/java/com/example/email/controller/CommodityController.java
@@ -18,9 +18,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
